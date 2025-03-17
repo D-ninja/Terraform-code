07275399dfbd2f886f8d236392996ca2f5d473c5
Pranjal Tripathi <72517705+D-ninja@users.noreply.github.com>
2025-03-17 12:03:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+  backend "s3" {
+    bucket  = "atlantis010"
+    key     = "atlantis/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "default"
+  }
+}
+resource "aws_instance" "my_ec2" {
+  ami="ami-0ac4dfaf1c5c0cce9"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "My_ec2ter"
+  }
+}
